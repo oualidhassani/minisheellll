7bcf1ec978d5cc16ca43073420b8169cd3d15ed3
oualid <oualidhassani215@gmail.com>
2024-05-07 13:59:30 +0200

    handle some error of cd

--- src/cd.c
+++ src/cd.c
@@ -1,13 +1,25 @@
 #include "../include/minishell.h"
 
+int morethan2arg(char **com)
+{
+    int i = 0;
+    int counter = 0;
+    while(com[i])
+    {
+        i++;
+        counter++;
+    }
+    return(counter);
+}
 void change_mydir(char *path)
 {
     char *cur;
     char buffer[PATH_MAX];
     cur = getcwd(buffer,PATH_MAX);
 
-    if(chdir(path) == 0)
-		set_myenv("OLDPWD", cur);
+    if(chdir(path) != 0)
+        perror("chdir");
+	set_myenv("OLDPWD", cur);
     set_myenv("PWD", path);
 }
 
@@ -16,12 +28,13 @@ void my_cd(char **com)
 {
     char *myhome = findmyvar("HOME");
 
+    int counter = morethan2arg(com);
+    if(counter > 2)
+        perror("chdir");
     if(com[1] == NULL)
         change_mydir(myhome);
     else
-    {
         change_mydir(com[1]);
-    }
 }
 
 void mypwd(void)
